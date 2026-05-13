5c302678dfb1870f6ff6d637325f17e877c18b35
Olamiji Bunday <oduboteolamiji@gmail.com>
2026-05-13 13:50:53 -0700

    new changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans, Geist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { url } from "inspector/promises";
 
 const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -12,9 +13,11 @@ const ibmSans = IBM_Plex_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Plus500BOT",
-  description: "Plus500BOT - secure, efficient and intelligent trading platform offering stocks, ETFS, crypto wallets, real-time market data, and AI powered auto-trading for investors. Manage trades, track portfolios, and make informed financial decisions covertly with ease, trading tools for a seamless investment experience.",
-  icons: {},
+  title: "AutoTraderInc",
+  description: "AutoTraderInc - secure, efficient and intelligent trading platform offering stocks, ETFS, crypto wallets, real-time market data, and AI powered auto-trading for investors. Manage trades, track portfolios, and make informed financial decisions covertly with ease, trading tools for a seamless investment experience.",
+  icons: {
+    icon: "app/favicon.ico",
+  },
   other: {
     'theme-color': '#040c29',
   },
